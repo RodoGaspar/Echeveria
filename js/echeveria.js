c4cd8f725d07c5d87faaa41c73103cec00475268
RodoGaspar <gasparianprds@gmail.com>
2024-04-08 16:54:46 -0300

    Estilo y js

---
+++ js/echeveria.js
@@ -0,0 +1,11 @@
+class producto {
+    constructor(id, nombre, imagen, descripcion, sol, agua, floracion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.imagen = imagen;
+        this.cantidad = descripcion;
+        this.descripcion = sol;
+        this.precio = agua;
+        this.precioTotal = floracion
+    }
+}
