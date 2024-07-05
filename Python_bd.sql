77874d72f5195225573f3cc42cdfebbcfe8f2ff9
marianeghi <166260767+marianeghi@users.noreply.github.com>
2024-07-05 18:11:17 -0300

    Add files via upload

---
+++ Python_bd.sql
@@ -0,0 +1,12 @@
+create database python_bd;
+
+use python_bd;
+
+create table usuarios(
+id int primary key,
+nombre varchar(20),
+apellido varchar(20),
+email varchar (20),
+usuario varchar (20)
+);
+
